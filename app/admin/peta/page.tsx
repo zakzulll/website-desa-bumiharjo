24aaf5c177fb6948c4a6de8c6f11538508d2c0b2
zakzulll <zakizulfikarm@gmail.com>
2026-09-16 11:29:55 +0700

    Update page.tsx

--- app/admin/peta/page.tsx
+++ app/admin/peta/page.tsx
@@ -243,7 +243,6 @@ export default function AdminPetaPage() {
                   <li><Link href="/admin/apb" className="nav-link">APB Desa</Link></li>
                 </ul>
               </li>
-              <li className="nav-item"><Link href="/admin/kontak" className="nav-link">Kontak</Link></li>
             </ul>
           </div>
 
